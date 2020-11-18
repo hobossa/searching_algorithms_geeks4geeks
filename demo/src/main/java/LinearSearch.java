25bacd9255b107de9d640e9f0e3a7407caabb93b
hobossa <hoboss@gmail.com>
2020-11-18 23:34:03 +0800

    done : LinearSearch

---
+++ demo/src/main/java/LinearSearch.java
@@ -0,0 +1,10 @@
+public class LinearSearch {
+    public static int search(int[] array, int v) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == v) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
---
+++ demo/src/test/java/LinearSearchTest.java
@@ -0,0 +1,17 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LinearSearchTest {
+
+    @Test
+    public void testSearch_ok() {
+        int[] arr = {10, 20, 80, 30, 60, 50, 110, 100, 130, 170};
+        Assert.assertEquals(6, LinearSearch.search(arr, 110));
+    }
+
+    @Test
+    public void testSearch_failed() {
+        int[] arr = {10, 20, 80, 30, 60, 50, 110, 100, 130, 170};
+        Assert.assertEquals(-1, LinearSearch.search(arr, 175));
+    }
+}
