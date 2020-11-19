c9cc401b9faa28d0750bf7d59229463834b87208
hobossa <hoboss@gmail.com>
2020-11-19 16:32:25 +0800

    opt ExponentialSearch

--- demo/src/main/java/ExponentialSearch.java
+++ demo/src/main/java/ExponentialSearch.java
@@ -4,17 +4,25 @@ public class ExponentialSearch {
     public static int search(int[] arr, int v) {
         int n = arr.length;
         if (n > 0) {
-            int lo = 0;
-            int range = 1;
-            int hi = min(lo + range, n);
-            while (lo < n) {
-                if (arr[lo] <= v && v<= arr[hi-1]) {
-                    return binarySearch(arr, v, lo, hi);
-                }
-                lo = hi;
-                range *= 2;
-                hi = min(lo+range, n);
+//            int lo = 0;
+//            int range = 1;
+//            int hi = min(lo + range, n);
+//            while (lo < n) {
+//                if (arr[lo] <= v && v<= arr[hi-1]) {
+//                    return binarySearch(arr, v, lo, hi);
+//                }
+//                lo = hi;
+//                range *= 2;
+//                hi = min(lo+range, n);
+//            }
+            if (v == arr[0]) {
+                return 0;
             }
+            int lo = 1;
+            while (lo < n && arr[lo] <= v) {
+                lo *= 2;
+            }
+            return binarySearch(arr, v, lo/2, min(lo, n));
         }
         return -1;
     }
