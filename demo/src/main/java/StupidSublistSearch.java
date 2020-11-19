27bbfb65facdad3890a152a6d371731d7d2a330c
hobossa <hoboss@gmail.com>
2020-11-19 17:19:04 +0800

    add StupidSublistSearch and KMPSublistSearchTest empty code

---
+++ demo/src/main/java/StupidSublistSearch.java
@@ -0,0 +1,13 @@
+public class StupidSublistSearch {
+    static class Node {
+        int data;
+        Node next;
+    }
+
+
+    // Returns true if first list is
+    // present in second list
+    static boolean findList(Node first, Node second) {
+        return false;
+    }
+}
