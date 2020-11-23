dc3c42e1eacab6cc0b3e571ce0ceb16440987050
hobossa <hoboss@gmail.com>
2020-11-23 19:36:17 +0800

    update test cases for FibonacciSearchTest

--- demo/src/test/java/FibonacciSearchTest.java
+++ demo/src/test/java/FibonacciSearchTest.java
@@ -6,13 +6,15 @@ import static org.junit.Assert.*;
 public class FibonacciSearchTest {
     @Test
     public void testSearch_ok() {
-        int [] arr = {2, 3, 4, 10, 40};
+        int[] arr = {2, 3, 4, 10, 40, 50, 60, 65, 78, 89, 99, 101, 121, 143, 169};
         Assert.assertEquals(3, FibonacciSearch.search(arr, 10));
+        Assert.assertEquals(0, FibonacciSearch.search(arr, 2));
+        Assert.assertEquals(13, FibonacciSearch.search(arr, 143));
     }
 
     @Test
     public void testSearch_fail() {
-        int [] arr = {2, 3, 4, 10, 40};
+        int[] arr = {2, 3, 4, 10, 40};
         Assert.assertEquals(-1, FibonacciSearch.search(arr, 11));
     }
 }
