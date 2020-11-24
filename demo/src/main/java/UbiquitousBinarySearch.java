f258bd5386f1f1862505e76186fa2bd937be5f57
hobossa <hoboss@gmail.com>
2020-11-24 20:06:59 +0800

    nope

--- demo/src/main/java/UbiquitousBinarySearch.java
+++ demo/src/main/java/UbiquitousBinarySearch.java
@@ -1,5 +1,63 @@
 public class UbiquitousBinarySearch {
+
+    // Given a sorted array of N distinct elements. Find a key in the array using least number of comparisons.
     public static int search(int[] arr, int k) {
+        return innerSearch(arr, k, 0, arr.length);
+    }
+
+    // In practice, comparison would be costly operation, it won’t be just primitive type comparison.
+    // It is more economical to minimize comparisons as that of theoretical limit.
+    // so we use if (>=) {} else {} rather than if (==) {}else if (>) {}else {}
+    static int innerSearch(int[] arr, int k, int lo, int hi) {
+        int m;
+        while (hi - lo > 1) {
+            m = (lo + hi) / 2;
+            if (k >= arr[m]) {
+                lo = m;
+            } else {
+                hi = m;
+            }
+        }
+
+        if (k == arr[lo]) {
+            return lo;
+        }
+
         return -1;
+    }
+
+
+    // Given an array of N distinct integers, find floor value of input 'key;.
+    // Say, A = {-1, 2, 3, 5, 6, 8, 9, 10} and key = 7, we should return 6 as outcome.
+    public static int floor(int[] arr, int k) {
+        if (k < arr[0]) {
+            return -1;
+        }
+        return innerFloor(arr, k, 0, arr.length);
+    }
+
+    static int innerFloor(int[] arr, int k, int lo, int hi) {
+        int m;
+        while (hi - lo > 1) {
+            m = (hi + lo) / 2;
+            if (k >= arr[m]) {
+                lo = m;
+            } else {
+                hi = m;
+            }
+        }
+        return arr[lo];
+    }
+
+
+    // Given a sorted array with possible duplicate elements. Find number of occurrences of input 'key' in log N time.
+    public static int count(int[] arr, int k) {
+        return 0;
+    }
+
+    // Given a sorted array of distinct elements, and the array is rotated at an unknown position.
+    // Find minimum element in the array.
+    public static int count(int[] arr) {
+        return 0;
     }
 }
--- demo/src/test/java/UbiquitousBinarySearchTest.java
+++ demo/src/test/java/UbiquitousBinarySearchTest.java
@@ -10,6 +10,7 @@ public class UbiquitousBinarySearchTest {
         Assert.assertEquals(5, UbiquitousBinarySearch.search(arr, 23));
         Assert.assertEquals(0, UbiquitousBinarySearch.search(arr, 2));
         Assert.assertEquals(9, UbiquitousBinarySearch.search(arr, 91));
+        Assert.assertEquals(4, UbiquitousBinarySearch.search(arr, 16));
     }
 
     @Test
@@ -18,5 +19,19 @@ public class UbiquitousBinarySearchTest {
         Assert.assertEquals(-1, UbiquitousBinarySearch.search(arr, 1));
         Assert.assertEquals(-1, UbiquitousBinarySearch.search(arr, 35));
         Assert.assertEquals(-1, UbiquitousBinarySearch.search(arr, 455));
+    }
+
+    @Test
+    public void testFloor_ok() {
+        int[] arr = {1, 2, 3, 5, 6, 8, 9, 10};
+        Assert.assertEquals(6, UbiquitousBinarySearch.floor(arr, 7));
+        Assert.assertEquals(10, UbiquitousBinarySearch.floor(arr, 13));
+        Assert.assertEquals(1, UbiquitousBinarySearch.floor(arr, 1));
+    }
+
+    @Test
+    public void testFloor_fail() {
+        int[] arr = {1, 2, 3, 5, 6, 8, 9, 10};
+        Assert.assertEquals(-1, UbiquitousBinarySearch.floor(arr, 0));
     }
 }
